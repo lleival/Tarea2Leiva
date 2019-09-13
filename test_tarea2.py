ff6f01b089afcdaab377d21e20e7501cae9c2556
lleival <lezzinll370@gmail.com>
2019-09-13 15:14:42 -0600

    Update test_tarea2.py
    Codigo corregido con Flake8

--- test_tarea2.py
+++ test_tarea2.py
@@ -1,10 +1,13 @@
 from SeccionPractica import tarea2
 
-def casoExito ():
-    assert tarea2(5,1) == [4,6,5]
-    
-def casoError1 ():
-    assert tarea2(5,8) == -1
 
-def casoError2 ():
-    assert tarea2("a",8) == -1
+def casoExito():
+    assert tarea2(5, 1) == [4, 6, 5]
+
+
+def casoError1():
+    assert tarea2(5, 8) == -1
+
+
+def casoError2():
+    assert tarea2("a", 8) == -1
