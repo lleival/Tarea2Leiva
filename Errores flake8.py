56b1940ae3c48b97f3700636f66b2b516f9c2739
lleival <lezzinll370@gmail.com>
2019-09-13 15:33:34 -0600

    Update Errores flake8.py
    Codigo corregido con Flake8

--- Errores flake8.py
+++ Errores flake8.py
@@ -1,8 +1,8 @@
-import random
 from SeccionPractica import tarea2
 
-def suma (A,B):
-    operaciones=tarea2(A,B)
-    respuesta=operaciones[1]
+
+def suma(A, B):
+    operaciones = tarea2(A, B)
+    respuesta = operaciones[1]
 
     return respuesta
